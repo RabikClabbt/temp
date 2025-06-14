ed8a8597019c17fe665d09c6484d3879ec4a301a
Yuta_Kubo <148171812+RabikClabbt@users.noreply.github.com>
2025-06-14 22:16:36 +0900

    Apply naming conventions to Java files

--- Java/RandomNumberGenerator.java
+++ Java/RandomNumberGenerator.java
@@ -1,4 +1,7 @@
-import java.util.*;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
 
 public class RandomNumberGenerator {
     public static void main(String[] args) {
